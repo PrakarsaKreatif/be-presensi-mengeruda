95cefd652a43578700cd51ed82f7a10360d25ac3
Thobiq <malyasyithobiq@gmail.com>
2026-09-22 10:59:57 +0700

    add cors configuration

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,36 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5178', 'http://localhost:5176'],
+    'allowed_origins' => [
+        'http://localhost:5173', 
+        'http://localhost:5174', 
+        'http://localhost:5175', 
+        'http://localhost:5176', 
+        'http://localhost:3000',
+        'http://localhost:3002',
+        'http://localhost:3003',
+        'http://127.0.0.1:5173',
+        'http://127.0.0.1:5174',
+        'http://127.0.0.1:5175',
+        'http://127.0.0.1:5176',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:3002',
+        'http://127.0.0.1:3003',
+        'https://sso.mengeruda.id',
+        'https://mengeruda.id',
+        'https://www.mengeruda.id',
+        'http://mengeruda.id',
+        'http://www.mengeruda.id',
+        'https://tourism.mengeruda.id',
+        'https://e-surat.mengeruda.id',
+        'http://e-surat.mengeruda.id',],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/.*\.mengeruda\.id$/',
+        '/^https?:\/\/mengeruda\.id$/',
+        '/^http:\/\/localhost:\d+$/',
+        '/^http:\/\/127\.0\.0\.1:\d+$/',
+    ],
 
     'allowed_headers' => ['*'],
 
